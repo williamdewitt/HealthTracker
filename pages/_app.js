9438ead978b41c8457426395d49881ea1b4d7518
William de Witt <sasilverain@hotmail.com>
2024-12-27 12:20:48 +0200

    feat: Add Clerk authentication middleware and update project configuration

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
 import '../styles/globals.css'
 
-  function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
-  }
+function MyApp({ Component, pageProps }) {
+  return (
+    <ClerkProvider>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  )
+}
 
-  export default MyApp
+export default MyApp
